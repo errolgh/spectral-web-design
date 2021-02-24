ef89fcf74a4d0c4fd80de15ed7cd72808e66e39a
errolgh <ewatson@ggoutfitters.com>
2021-02-24 10:33:59 -0500

    animation hotfix

--- src/components/home/WhatWeDo.js
+++ src/components/home/WhatWeDo.js
@@ -17,27 +17,35 @@ export default function WhatWeDo(){
     }
     return(
         <section className="what-we-do">
-            <motion.div className="what-we-do-top" variants={pageAnimation} initial="hidden" animate={controls} ref={element}>
+            {/* <motion.div className="what-we-do-top" variants={pageAnimation} initial="hidden" animate={controls} ref={element}> */}
+            <div className="what-we-do-top">
                 <h2>What We Do</h2>
                 <span>~* *~</span>
                 <p>We are a nation-wide web development and design company based in Baltimore, MD. Our services cater to small businesses and professional individuals who want to grow and understand that a unique web presence is imperative in the age of cookie-cutter WordPress and Wix sites. <Link to="/pricing">LEARN MORE</Link></p>
-            </motion.div>                                                                                     
+            </div>
+            {/* </motion.div>                                                                                      */}
             <div className="what-we-do-content">
-                <motion.div className="item" variants={leftToRight} initial="hidden" animate={controls} ref={element}>
+                {/* <motion.div className="item" variants={leftToRight} initial="hidden" animate={controls} ref={element}> */}
+                <div className="item">
                     <img src={devices} alt="devices"/>
                     <h3>Mobile-Responsive Design</h3>
                     <p>We understand that in today's digital landscape, your web presence needs to be designed for desktop, mobile devices, and everything in between.</p>
-                </motion.div>
-                <motion.div className="item" variants={fromTheBottom} initial="hidden" animate={controls} ref={element}>
+                </div>
+                {/* </motion.div> */}
+                {/* <motion.div className="item" variants={fromTheBottom} initial="hidden" animate={controls} ref={element}> */}
+                <div className="item">
                     <img src={three_up_arrows} alt="three up arrows"/>
                     <h3>SEO-Smart Performance</h3>
                     <p>We test website load times, accessibility, SEO-Keyword quality, and performance on every website. The metrics modern search engines use to determine where your site shows up in search results.</p>
-                </motion.div>
-                <motion.div className="item" variants={rightToLeft} initial="hidden" animate={controls} ref={element}>
+                </div>
+                {/* </motion.div> */}
+                {/* <motion.div className="item" variants={rightToLeft} initial="hidden" animate={controls} ref={element}> */}
+                <div className="item">
                     <img src={nerd} alt="nerd"/>
                     <h3>Hosting & Domain Maintenance</h3>
                     <p>Leave domain, hosting, and encryption to your techology professionals. Our service is meant to help you focus on your business -- leave the nerdy stuff to us.</p>
-                </motion.div>
+                </div>
+                {/* </motion.div> */}
             </div>
         </section>
     )
